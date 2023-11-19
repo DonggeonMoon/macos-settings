4033759dc1920e7ed245b5cb9aebc99bd2b4599a
DonggeonMoon <nullsafer@naver.com>
2023-11-20 01:43:37 +0900

    feat: -y 옵션 추가

--- macos-settings.sh
+++ macos-settings.sh
@@ -1,17 +1,17 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install git
-brew install node
-brew install python@3.10
-brew install adoptopenjdk/openjdk/adoptopenjdk8
-brew install adoptopenjdk/openjdk/adoptopenjdk11
-brew install adoptopenjdk/openjdk/adoptopenjdk17
+brew install git -y
+brew install node -y
+brew install python@3.10 -y
+brew install adoptopenjdk/openjdk/adoptopenjdk8 -y
+brew install adoptopenjdk/openjdk/adoptopenjdk11 -y
+brew install adoptopenjdk/openjdk/adoptopenjdk17 -y
 
-brew install --cask docker
+brew install --cask docker -y
 
-brew install --cask heidisql
+brew install --cask heidisql -y
 
-brew install --cask visual-studio-code
+brew install --cask visual-studio-code -y
 
-brew install --cask google-chrome
-brew install --cask firefox
+brew install --cask google-chrome -y
+brew install --cask firefox -y
