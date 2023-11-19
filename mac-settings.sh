f142b52ffaf80ac0f539ecf0f7c052a3c5f95fab
Donggeon Moon <nullsafer@naver.com>
2023-11-19 23:54:31 +0900

    feat: initial commit

---
+++ mac-settings.sh
@@ -0,0 +1,17 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
+brew install node
+brew install python@3.10
+brew install adoptopenjdk/openjdk/adoptopenjdk8
+brew install adoptopenjdk/openjdk/adoptopenjdk11
+brew install adoptopenjdk/openjdk/adoptopenjdk17
+
+brew install --cask docker
+
+brew install --cask heidisql
+
+brew install --cask visual-studio-code
+
+brew install --cask google-chrome
+brew install --cask firefox
